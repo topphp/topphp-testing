652965b5e0b1ca75f94d50487745339b785c7543
sleep <sleep@kaituocn.com>
2020-02-11 00:05:35 +0800

    协程命令行工具生成

---
+++ src/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 凯拓软件 [临渊羡鱼不如退而结网,凯拓与你一同成长]
+ * Project: topphp-testing
+ * Date: 2020/2/10 20:00
+ * Author: sleep <sleep@kaituocn.com>
+ */
+declare(strict_types=1);
+
+namespace Topphp\TopphpTesting;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
