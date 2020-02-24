b2d9922cd4b5dbd6e6ff47e4c7cf0507a1acb0be
baiwenbiao <344147805@qq.com>
2020-02-24 16:04:48 +0800

    单元测试，评审版，还有一个关于swoole的问题没解决

--- src/TestCase.php
+++ src/TestCase.php
@@ -9,20 +9,32 @@ declare(strict_types=1);
 
 namespace Topphp\TopphpTesting;
 
+use Topphp\TopphpClient\Client;
+use Topphp\TopphpClient\guzzle\GuzzleClient;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected $app;
+    protected static $config;
+    /** @var GuzzleClient */
+    protected static $httpClient;
 
     public function __construct($name = null, array $data = [], $dataName = '')
     {
         if (class_exists(\think\App::class)) {
-            $this->app = new \think\App();
-            $http      = $this->app->http;
-            $response  = $http->run();
+            $this->app            = new \think\App();
+            $http                 = $this->app->http;
+            $response             = $http->run();
+            self::$config['Http'] = $this->app->config->get("topphpClientHttp");
+        }
+        if (empty(self::$config['Http'])) {
+            $configDir            = dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR;
+            self::$config['Http'] = include $configDir . "topphpClientHttp.php";
         }
         parent::__construct($name, $data, $dataName);
         if (extension_loaded('swoole')) {
             \Swoole\Runtime::enableCoroutine(true);
         }
+        self::$httpClient = Client::getInstance(self::$config['Http'])->cli("http");
     }
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,15 +9,107 @@ use Topphp\TopphpTesting\HttpTestCase;
 class ExampleTest extends HttpTestCase
 {
 
+    /**
+     * 测试 GET 请求
+     * @return array|bool
+     * @author bai
+     */
     public function testHttpGetRequest()
     {
-        $domain = "http://39.100.41.225:8805/";
-        $requestUrl = "http://39.100.41.225:8805/TestBai/Guzzle";
-
+        $errorMsg   = null;
+        $requestUrl = "http://www.baidu.com";
+        $headers    = [];
+        $res        = self::$httpClient->get($requestUrl, $headers);
+        if ($res === false) {
+            // 错误信息通过这种方式获取
+            $errorMsg = self::$httpClient->getErrorMsg();
+        }
+        $this->assertTrue($errorMsg === null);
+        return $res;
     }
 
+    /**
+     * 测试 POST 请求
+     * @return array|bool
+     * @author bai
+     */
     public function testHttpPostRequest()
     {
+        $errorMsg   = null;
+        $requestUrl = "http://www.baidu.com";
+        $param      = [
+            "id" => 1
+        ];
+        $headers    = [];
+        $res        = self::$httpClient->post($requestUrl, $param, 'json', $headers);
+        if ($res === false) {
+            // 错误信息通过这种方式获取
+            $errorMsg = self::$httpClient->getErrorMsg();
+        }
+        $this->assertTrue($errorMsg === null);
+        return $res;
+    }
 
+    /**
+     * 测试 PUT 请求
+     * @return array|bool
+     * @author bai
+     */
+    public function testHttpPutRequest()
+    {
+        $errorMsg   = null;
+        $requestUrl = "http://www.baidu.com";
+        $param      = [
+            "id" => 1
+        ];
+        $res        = self::$httpClient->put($requestUrl, $param);
+        if ($res === false) {
+            // 错误信息通过这种方式获取
+            $errorMsg = self::$httpClient->getErrorMsg();
+        }
+        $this->assertTrue($errorMsg === null);
+        return $res;
+    }
+
+    /**
+     * 测试 PATCH 请求
+     * @return array|bool
+     * @author bai
+     */
+    public function testHttpPatchRequest()
+    {
+        $errorMsg   = null;
+        $requestUrl = "http://www.baidu.com";
+        $param      = [
+            "id" => 1
+        ];
+        $res        = self::$httpClient->patch($requestUrl, $param);
+        if ($res === false) {
+            // 错误信息通过这种方式获取
+            $errorMsg = self::$httpClient->getErrorMsg();
+        }
+        $this->assertTrue($errorMsg === null);
+        return $res;
+    }
+
+    /**
+     * 测试 DELETE 请求
+     * @return array|bool
+     * @author bai
+     */
+    public function testHttpDeleteRequest()
+    {
+        $errorMsg   = null;
+        $requestUrl = "http://www.baidu.com";
+        $param      = [
+            "id" => 1
+        ];
+        $res        = self::$httpClient->get($requestUrl, $param);
+        if ($res === false) {
+            // 错误信息通过这种方式获取
+            $errorMsg = self::$httpClient->getErrorMsg();
+        }
+        $this->assertTrue($errorMsg === null);
+        return $res;
     }
 }
